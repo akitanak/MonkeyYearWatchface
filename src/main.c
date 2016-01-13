53491858cb776cce0da37014e02d4b29d450cf44
Akihito <akihito_tanaka@me.com>
2016-01-14 01:32:22 +0900

    add weather function

--- src/main.c
+++ src/main.c
@@ -20,6 +20,24 @@ static void update_time() {
   text_layer_set_text(s_time_layer, s_buffer);
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void main_window_load(Window *window) {
 
   Layer *window_layer = window_get_root_layer(window);
@@ -47,7 +65,7 @@ static void main_window_load(Window *window) {
 
   // update current time
   update_time();
-  
+
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
@@ -77,6 +95,12 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
 
 }
 
